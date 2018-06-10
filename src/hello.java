55430e3f8544c53c062199990661c9ae6d5b0bbd
Arkadiusz Szałaj <arszalaj@gmail.com>
2018-06-10 19:35:24 +0200

    loop added

--- src/hello.java
+++ src/hello.java
@@ -1,19 +1,21 @@
 import java.util.Scanner;
 
-import com.example.rpn.RPN;
+import com.salatsoft.rpn.RPN;
 
 public class hello {
 
 	static RPN rpn = new RPN();
 	public static void main(String [] args)
 	  {
-		System.out.println("Write your expression down, please.");
-		Scanner input = new Scanner(System.in);
-		String expression = input.next();
-		rpn.infixToRPN(expression);
-		rpn.evaluateRPN(rpn.rpn, true);
-		System.out.println(rpn.result);
-		System.out.println(rpn.rpn);
+		while(true) {
+			System.out.println("Write your expression down, please.");
+			Scanner input = new Scanner(System.in);
+			String expression = input.next();
+			rpn.infixToRPN(expression);
+			rpn.evaluateRPN(rpn.rpn, true);
+			System.out.println("result:\t" + rpn.result);
+			System.out.println("RPN:\t" + rpn.rpn);
+		}
 	  }
 
 }
